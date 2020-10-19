2d3b64faed8009762f55c1b00907a2ac67c6279b
JacquesEngelbrecht <jacquesjengelbrecht@gmail.com>
2020-10-19 08:40:50 +0200

    First commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,15 @@
+<div class="footer">
+    <p>Footer</p>
+</div>
+
+<style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: lightgray;
+        color: white;
+        text-align: center;
+    }
+</style>
