fb94f4a414679e0ed591aecee8b723d04001bd55
JacquesEngelbrecht <jacquesjengelbrecht@gmail.com>
2020-10-20 09:41:00 +0200

    Login/Logout function added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use Session;
 
 class ProductController extends Controller
 {
@@ -20,5 +22,24 @@ class ProductController extends Controller
     function search(Request $request){
         $data = Product::where('name', 'like', '%'.$request->input('query').'%')->get();
         return view('search', ['products'=>$data]);
+    }
+
+    function addToCart(Request $request) {
+        if($request->session()->has('user')) {
+            $cart = new Cart;
+            $cart->user_id = $request->session()->get('user')['id'];
+            $cart->product_id = $request->product_id;
+            $cart->save();
+            //notify()->success('Product added successfully!');
+            return redirect('/'); 
+        } else {
+            //notify()->success('You need to be logged in to add to cart!');
+            return redirect('/login');
+        }
+    }
+
+    static function cartItem() {
+        $user_id = Session::get('user')['id'];
+        return Cart::where('user_id', $user_id)->count();
     }
 }
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,5 +1,12 @@
+<?php 
+use App\Http\Controllers\ProductController;
+$cartTotal = 0;
+if(Session::has('user')) {
+  $cartTotal = ProductController::cartItem();
+}
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/">(Your Name Here)</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,7 +14,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
+        <a class="nav-link" href="/">Home</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Orders</a>
@@ -15,13 +22,26 @@
     </ul>
     <ul class="navbar-nav navbar-right">
       <li>
-        <a href="#">Cart(0)</a>
+        <a href="#">Cart({{$cartTotal}})</a>
       </li>    
     </ul>
     <form action="/search" class="form-inline navbar-form navbar-left">
       <input name="query" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
-    
+    <ul class="navbar-nav navbar-right">
+      @if(Session::has('user'))
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          {{Session::get('user')['name']}}
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="/logout">Log out</a>
+        </div>
+      </li>
+      @else 
+      <li><a href="/login">Login</a></li>
+      @endif
+    </ul>
   </div>
 </nav>
